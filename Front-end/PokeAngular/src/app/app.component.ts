88162ea582a586b005ffa8bdb2826068fd996bb9
DAM <mariocriadoguerrero26@gmail.com>
2025-02-17 16:29:05 +0100

    added set

--- Front-end/PokeAngular/src/app/app.component.ts
+++ Front-end/PokeAngular/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SetComponent } from './set/set.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SetComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Front-end/PokeAngular/src/app/set/set.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-set',
+  standalone: true,
+  imports: [],
+  templateUrl: './set.component.html',
+  styleUrl: './set.component.css'
+})
+export class SetComponent implements OnInit {
+  boosterPack = {
+    data: {
+      id: "swsh1",
+      name: "Sword & Shield",
+      series: "Sword & Shield",
+      printedTotal: 202,
+      total: 216,
+      legalities: {
+        unlimited: "Legal",
+        standard: "Legal",
+        expanded: "Legal"
+      },
+      ptcgoCode: "SSH",
+      releaseDate: "2020/02/07",
+      updatedAt: "2020/08/14 09:35:00",
+      images: {
+        symbol: "https://images.pokemontcg.io/swsh1/symbol.png",
+        logo: "https://images.pokemontcg.io/swsh1/logo.png"
+      }
+    }
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
