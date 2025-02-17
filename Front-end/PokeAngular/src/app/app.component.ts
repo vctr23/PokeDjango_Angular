ac1796f8dac9062bc467eb0bdad74b7e85acd2dd
alexEsteban19 <alesteban1608@gmail.com>
2025-02-17 16:22:31 +0100

    added header

--- Front-end/PokeAngular/src/app/app.component.ts
+++ Front-end/PokeAngular/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderAngularComponent } from './header-angular/header-angular.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderAngularComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Front-end/PokeAngular/src/app/header-angular/header-angular.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-angular',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-angular.component.html',
+  styleUrl: './header-angular.component.css'
+})
+export class HeaderAngularComponent {
+
+}
