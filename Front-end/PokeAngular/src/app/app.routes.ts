e702aae4114e80e8ce643b055a291313e14dc99f
albxrtx <albertomariannechita@gmail.com>
2025-02-18 18:09:59 +0100

    css fixed

--- Front-end/PokeAngular/src/app/app.routes.ts
+++ Front-end/PokeAngular/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { SetComponent } from './set/set.component';
+import { InventoryComponent } from './inventory/inventory.component';
+export const routes: Routes = [
+  { path: '', component: SetComponent },
+  { path: 'inventory', component: InventoryComponent },
+];
