7dfa6125a0c99c1c7792417b72d9e0c25fec7874
alexEsteban19 <alesteban1608@gmail.com>
2025-02-21 21:28:43 +0100

    login(not ended)

--- Front-end/PokeAngular/src/app/app.routes.ts
+++ Front-end/PokeAngular/src/app/app.routes.ts
@@ -2,8 +2,11 @@ import { Routes } from '@angular/router';
 import { SetComponent } from './set/set.component';
 import { InventoryComponent } from './inventory/inventory.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
 export const routes: Routes = [
   { path: '', component: SetComponent },
   { path: 'inventory', component: InventoryComponent },
   { path: 'signup', component: SignUpComponent },
+  { path: 'login', component: LoginComponent },
+
 ];
