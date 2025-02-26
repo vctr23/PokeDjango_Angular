94dc30b9d74de50ff8b5cee2b204b6546c382f24
albxrtx <albertomariannechita@gmail.com>
2025-02-26 17:51:54 +0100

    expansiones

--- Front-end/PokeAngular/src/app/expansions/expansions.component.ts
+++ Front-end/PokeAngular/src/app/expansions/expansions.component.ts
@@ -3,22 +3,22 @@ import { isPlatformBrowser } from '@angular/common';
 import { ServiceService } from '../services/service.service';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-expansions',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, FormsModule],
   templateUrl: './expansions.component.html',
   styleUrl: './expansions.component.css',
   providers: [ServiceService],
 })
 export class ExpansionsComponent implements OnInit {
   cards: any[] = [];
+  searchName: string = '';
+  searchNumber: string = '';
 
-  constructor(
-    private service: ServiceService,
-    @Inject(PLATFORM_ID) private platformId: Object // Inyectamos el identificador de plataforma
-  ) {}
+  constructor(private service: ServiceService) {}
 
   ngOnInit(): void {
     this.service.getCardsSet1().subscribe(
@@ -29,6 +29,15 @@ export class ExpansionsComponent implements OnInit {
       (error) => {
         console.error('Error al obtener las cartas:', error);
       }
+    );
+  }
+  get filteredCards() {
+    return this.cards.filter(
+      (card) =>
+        card.name.toLowerCase().includes(this.searchName.toLowerCase()) ||
+        (this.searchNumber !== null &&
+          this.searchNumber !== '' &&
+          card.number === Number(this.searchNumber))
     );
   }
 }
